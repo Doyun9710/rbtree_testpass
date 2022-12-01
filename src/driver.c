04c5f97a1e882f82ab07e2794c1f0e9acc41adbf
Doyun9710 <doyun1010k@gmail.com>
2022-12-01 10:52:33 +0900

    Passed All Test

--- src/driver.c
+++ src/driver.c
@@ -1,5 +1,22 @@
 #include "rbtree.h"
+#include <stdio.h>
+// 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
+int main(int argc, char *argv[])
+{
+    // rbtree *t = new_rbtree();
+    // key_t entries[] = {10, 5, 8, 34, 67, 23, 156, 24, 2, 12, 24, 36, 990, 25};
+    // const size_t n = sizeof(entries) / sizeof(entries[0]);
 
-int main(int argc, char *argv[]) {
+    // for (int i = 0; i < n; i++) {
+    //     // printf("%d\n", entries[i]);
+    //     rbtree_insert(t, entries[i]);
+    // }
 
+    // key_t *res = calloc(n, sizeof(key_t));
+
+    // rbtree_to_array(t, res, n);
+
+    // printf("succes!! \n");
+
+    // return 0;
 }
